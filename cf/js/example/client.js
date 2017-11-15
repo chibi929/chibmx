79dd77ba557af365892db419807042f29036f90f
chibi929 <ibihc929@gmail.com>
2017-11-15 21:16:04 +0900

    Refactor reconnect.

--- cf/js/example/client.js
+++ cf/js/example/client.js
@@ -1,21 +1,29 @@
 var WebSocket = require('ws');
 var ws;
 
-connect();
+function reconnect() {
+  ws = null;
+  console.log("[CLIENT] 3秒後に再接続します。");
+  setTimeout(function() {
+    connect();
+  }, 3000);
+}
 
 function connect() {
   ws = new WebSocket('ws://localhost:3000/');
-  console.log("foo");
+  console.log("[CLIENT] connect");
 
-  ws.on('open', function open() {
-    console.log("OPEN!");
+  ws.on('open', function() {
+    console.log("[CLIENT] open");
     ws.send("Hello");
-  }).on('error', function open(err) {
-    console.log("ERROR!: " + err);
-    connect();
+  }).on('error', function(err) {
+    console.log(`[CLIENT] error: ${err}`);
   }).on('message', function(message) {
-    console.log("CLIENT received: %s", message);
-  }).on('close', function() {
-    console.log("CLIENT close");
+    console.log(`[CLIENT] message: ${message}`);
+  }).on('close', function(code, reason) {
+    console.log(`[CLIENT] close: code = ${code}, reason = ${reason}`);
+    reconnect();
   });
 }
+
+connect();
