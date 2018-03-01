85cad2fbf17fa1dda22e44f6d99419919ebc08a1
chibi929 <ibihc929@gmail.com>
2018-03-01 00:00:27 +0000

    Implement pickup-quiz.

--- kinoko/src/pickup-quiz.ts
+++ kinoko/src/pickup-quiz.ts
@@ -5,6 +5,7 @@ type Quiz = string;
 
 export class PickupQuiz {
   private readonly pickupQuizzes: Quiz[];
+  private currentIndex: number = 0;
 
   constructor(
     private readonly quizzes: Quiz[],
@@ -12,14 +13,22 @@ export class PickupQuiz {
   ) {
     this.pickupQuizzes = [].concat(quizzes);
     this.pickupQuizzes.shuffle();
-    
+
     if (this.pickupQuizzes.length > count) {
       this.pickupQuizzes.length = count;
     }
   }
 
-  debug(): void {
-    console.log(`quizzes: ${this.quizzes}`);
-    console.log(`pickupQuizzes: ${this.pickupQuizzes}`);
+  hasNext(): boolean {
+    return this.currentIndex + 1 < this.pickupQuizzes.length;
+  }
+
+  getCurrentQuiz(): Quiz {
+    return this.pickupQuizzes[this.currentIndex];
+  }
+
+  nextQuiz(): Quiz {
+    this.currentIndex++;
+    return this.pickupQuizzes[this.currentIndex];
   }
 }
