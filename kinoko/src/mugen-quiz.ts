a9c3e8fedd1b44dc295e505a20c46cbecbc4012e
chibi929 <ibihc929@gmail.com>
2018-03-01 15:59:19 +0000

    Add method comment.

--- kinoko/src/mugen-quiz.ts
+++ kinoko/src/mugen-quiz.ts
@@ -1,22 +1,49 @@
 // 本当はちゃんと実装する
 type Quiz = string;
 
+/**
+ * 無限ループ用クイズクラス
+ * 現行のシステムの一部をクラスに分離したもの
+ */
 export class MugenQuiz {
+  /**
+   * コンストラクタ
+   *
+   * @param quizzes クイズオブジェクト配列
+   * @param currentIndex 現在のインデックス(途中から開始可能)
+   */
   constructor(
     private readonly quizzes: Quiz[],
     private currentIndex: number
   ) {
   }
 
+  /**
+   * 次のクイズがあるかどうか
+   *
+   * @returns 必ず true を返す
+   */
   hasNext(): boolean {
     return true;
   }
 
+  /**
+   * 現在のクイズオブジェクトを返す
+   * - 無限ループするので剰余算で index を決定する
+   *
+   * @returns クイズオブジェクト
+   */
   getCurrentQuiz(): Quiz {
     const index = this.currentIndex % this.quizzes.length;
     return this.quizzes[index];
   }
 
+  /**
+   * 次のクイズに進める
+   * - 無限ループするので剰余算で index を決定する
+   *
+   * @returns 次のクイズオブジェクト
+   */
   nextQuiz(): Quiz {
     this.currentIndex++;
     const index = this.currentIndex % this.quizzes.length;
--- kinoko/src/pickup-quiz.ts
+++ kinoko/src/pickup-quiz.ts
@@ -3,10 +3,20 @@ import './extension/array.extension';
 // 本当はちゃんと実装する
 type Quiz = string;
 
+/**
+ * ピックアップ用クイズクラス
+ */
 export class PickupQuiz {
   private readonly pickupQuizzes: Quiz[];
   private currentIndex: number = 0;
 
+  /**
+   * コンストラクタ
+   * - シャッフルを行ってピックアップ分だけ抽出する
+   *
+   * @param quizzes クイズオブジェクト配列
+   * @param count ピックアップ数
+   */
   constructor(
     private readonly quizzes: Quiz[],
     private readonly count: number
@@ -19,14 +29,29 @@ export class PickupQuiz {
     }
   }
 
+  /**
+   * 次のクイズがあるかどうか
+   *
+   * @returns true: ある, false: ない
+   */
   hasNext(): boolean {
     return this.currentIndex + 1 < this.pickupQuizzes.length;
   }
 
+  /**
+   * 現在のクイズオブジェクトを返す
+   *
+   * @returns クイズオブジェクト
+   */
   getCurrentQuiz(): Quiz {
     return this.pickupQuizzes[this.currentIndex];
   }
 
+  /**
+   * 次のクイズに進める
+   *
+   * @returns 次のクイズオブジェクト
+   */
   nextQuiz(): Quiz {
     this.currentIndex++;
     return this.pickupQuizzes[this.currentIndex];
