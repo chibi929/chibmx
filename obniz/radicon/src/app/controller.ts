2f6da79d7a6da2c083f8f8098d707360f857d4dc
chibi929 <ibihc929@gmail.com>
2018-04-28 08:14:06 +0900

    Add type declare.

--- obniz/radicon/src/app/controller.ts
+++ obniz/radicon/src/app/controller.ts
@@ -1,8 +1,24 @@
 declare const Obniz: any;
+declare class Led {
+  on(): void;
+  off(): void;
+  output(on: boolean): void;
+  blink(intervalMS): void;
+  endBlink(): void;
+}
+declare class DCMotor {
+  forward(): void;
+  reverse(): void;
+  stop(): void;
+  move(forward: boolean): void;
+  power(power: number): void;
+}
 
 export class Controller {
   private readonly obniz: any;
-  private led: any;
+  private led: Led;
+  private motorLeft: any;
+  private motorRight: any;
 
   constructor(obnizId: string, connectedCallback?: () => void) {
     this.obniz = new Obniz(obnizId);
@@ -11,6 +27,8 @@ export class Controller {
       console.log('onconnect');
       connectedCallback && connectedCallback();
       this.led = this.obniz.wired('LED', { anode: 0, cathode: 1 });
+      this.motorLeft = this.obniz.wired('DCMotor', { forward: 2, back: 3 });
+      this.motorRight = this.obniz.wired('DCMotor', { forward: 4, back: 5 });
     };
   }
 
