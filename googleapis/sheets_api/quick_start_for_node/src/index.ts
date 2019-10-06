60ecab26d957103f10fad8f96bad00a9d0bb255d
chibi929 <ibihc929@gmail.com>
2019-10-06 17:41:22 +0900

    Fix

--- googleapis/sheets_api/quick_start_for_node/src/index.ts
+++ googleapis/sheets_api/quick_start_for_node/src/index.ts
@@ -2,6 +2,32 @@ import * as fs from 'fs';
 import * as readline from 'readline';
 import { google } from 'googleapis';
 
+interface ICredentials {
+  installed: {
+    client_id: string;
+    project_id: string;
+    auth_uri: string;
+    token_uri: string;
+    auth_provider_x509_cert_url: string;
+    client_secret: string;
+    redirect_uris: string[];
+  };
+}
+
+/**
+ * 1. credentials.json を読み込む
+ * 2. credentials.json の client_id, client_secret を使って OAuth2Client を作成する
+ * 3. token.json の有無で処理が分岐する
+ *     - token.json がある場合
+ *         1. そのまま OAuth2Client を設定する
+ *     - token.json がない場合
+ *         1. SCOPE を使って 認証URL を作成する
+ *         2. 認証キーを入力するための標準入力を準備する
+ *         3. ユーザーに code を入力させて新しいトークンを取得する
+ *         4. token.json に書き込む
+ * 4. Spread Sheet にアクセスする
+ */
+
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
@@ -13,7 +39,7 @@ const TOKEN_PATH = 'token.json';
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Sheets API.
-  authorize(JSON.parse(content), listMajors);
+  authorize(JSON.parse(content.toString()), listMajors);
 });
 
 /**
@@ -22,14 +48,14 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
+function authorize(credentials: ICredentials, callback: Function) {
   const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
     if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
+    oAuth2Client.setCredentials(JSON.parse(token.toString()));
     callback(oAuth2Client);
   });
 }
