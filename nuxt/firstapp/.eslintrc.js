f84d6cfb7c905689d38742b70aaffefbb5918c5e
chibi929 <ibihc929@gmail.com>
2019-11-08 11:49:05 +0900

    Add modules of '@nuxtjs/eslint-config-typescript'.

--- nuxt/firstapp/.eslintrc.js
+++ nuxt/firstapp/.eslintrc.js
@@ -5,20 +5,36 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@typescript-eslint/parser'
   },
   extends: [
     '@nuxtjs',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    '@nuxtjs/eslint-config-typescript'
   ],
   plugins: [
-    'prettier'
+    'prettier',
+    '@typescript-eslint'
   ],
   // add your custom rules here
   rules: {
-    'nuxt/no-cjs-in-config': 'off'
+    'nuxt/no-cjs-in-config': 'off',
+    // note you must disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", {
+      "vars": "all",
+      "args": "after-used",
+      "ignoreRestSiblings": false
+    }],
+    "space-before-function-paren": ["error", "never"],
+    // https://github.com/vuejs/eslint-plugin-vue/blob/master/docs/rules/html-self-closing.md
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always"
+      }
+    }]
   }
 }
