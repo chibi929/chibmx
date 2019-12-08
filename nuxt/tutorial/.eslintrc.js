34dd2bfec213d1807a3be509d93270d0451764ea
chibi929 <ibihc929@gmail.com>
2019-12-08 21:16:49 +0900

    Lint.

--- nuxt/tutorial/.eslintrc.js
+++ nuxt/tutorial/.eslintrc.js
@@ -4,19 +4,15 @@ module.exports = {
     browser: true,
     node: true
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   extends: [
     '@nuxtjs',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    '@nuxtjs/eslint-config-typescript'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
     'nuxt/no-cjs-in-config': 'off'
